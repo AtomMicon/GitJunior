e77547a529969b08b96fb5b919e9bd41595db9a0
Atom <atom.micon@gmail.com>
2025-08-20 16:11:04 +0600

    Script init

---
+++ Assets/Scripts/Grower.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Grow : MonoBehaviour
+{
+    [SerializeField] private float _growingSpeed;
+
+    private void Update()
+    {
+        transform.localScale += Vector3.one * _growingSpeed * Time.deltaTime;
+    }
+}
